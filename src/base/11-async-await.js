ffe87124f40d27ee9dbe273e41f330e165b1e315
Jorman <espinoza.dev@gmail.com>
2021-01-01 20:23:33 -0300

    Testing async-await

--- src/base/11-async-await.js
+++ src/base/11-async-await.js
@@ -3,30 +3,19 @@
 // const getImagenPromesa = () => new Promise( resolve => resolve('https://ajskdhaskjdhajs.com') )
 // getImagenPromesa().then( console.log );
 
-const getImagen = async() => {
+export const getImagen = async() => {
 
     try {
-
-        const apiKey = 'C1khQe3Z7R1W2lfTO9myKeuShdqFYSGC';
+        const apiKey = 'a6gn2SGDDwcxAX3N4XEWhwClj4Vx6s9h';
         const resp   = await fetch(`http://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`);
-        const { data } = await resp.json(); 
+        const { data } = await resp.json();
 
         const { url } = data.images.original;
 
-        const img = document.createElement('img');
-        img.src = url;
-        document.body.append( img );
-
+        return url;
     } catch (error) {
         // manejo del error
-        console.error(error)
+        // console.error(error)
+        return error;
     }
-    
-    
-    
 }
-
- getImagen();
-
-
-
