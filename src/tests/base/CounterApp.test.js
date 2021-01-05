55932e2779149024e0262fc90683eb0e864c885d
Jorman <espinoza.dev@gmail.com>
2021-01-05 09:37:07 -0300

    Ending component unit testing

--- src/tests/base/CounterApp.test.js
+++ src/tests/base/CounterApp.test.js
@@ -4,16 +4,44 @@ import { shallow } from 'enzyme';
 import CounterApp from "../../CounterApp";
 
 describe('Pruebas en <CounterApp />', () => {
+
+    let wrapper = shallow(<CounterApp />);
+
+    beforeEach(() => {
+        wrapper = shallow(<CounterApp />);
+    });
+
     test('Debe mostrar el <CounterApp /> correctamente', () => {
-        const wrapper = shallow(<CounterApp />);
         expect(wrapper).toMatchSnapshot();
     });
 
     test('Debe mostrar el valor por defecto/inicial 100', () => {
-        const value = 100;
-        const wrapper = shallow(<CounterApp value={value} />);
+        const wrapper = shallow(<CounterApp value={100} />);
         const counterText = wrapper.find('h2').text().trim();
 
-        expect(counterText).toBe(`${value}`);
+        expect(counterText).toBe('100');
+    });
+
+    test('Debe incrementar con el botón +1', () => {
+        wrapper.find('button').at(0).simulate('click');
+        const counterText = wrapper.find('h2').text().trim();
+
+        expect(counterText).toBe('11');
+    });
+
+    test('Debe incrementar con el botón -1', () => {
+        wrapper.find('button').at(2).simulate('click');
+        const counterText = wrapper.find('h2').text().trim();
+
+        expect(counterText).toBe('9');
+    });
+
+    test('Debe reiniciar valor por defecto con el botón reset', () => {
+        const wrapper = shallow(<CounterApp value={25} />);
+        wrapper.find('button').at(0).simulate('click');
+        wrapper.find('button').at(1).simulate('click');
+        const counterText = wrapper.find('h2').text().trim();
+
+        expect(counterText).toBe('25');
     });
 });
